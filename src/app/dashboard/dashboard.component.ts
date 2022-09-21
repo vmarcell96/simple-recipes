36ecc80d96a4c6da533110e5239ae9209ab24d4c
vmarcell96 <vajdam96@gmail.com>
2022-09-21 13:09:03 +0200

    added dashboard component for displaying top recipes

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe';
+import { RecipeService } from '../recipe.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  recipes: Recipe[] = [];
+  constructor(private recipeService: RecipeService) { }
+
+  ngOnInit(): void {
+    this.getRecipes();
+  }
+
+  getRecipes(): void {
+    this.recipeService.getRecipes()
+        .subscribe(recipes => this.recipes = recipes.slice(0,3));
+  }
+
+}
