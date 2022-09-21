273c12d0fc11abcf5223978a8510b7bfdf0b9bf7
vmarcell96 <vajdam96@gmail.com>
2022-09-21 17:23:38 +0200

    implemented CRUD operations, error handling search feature in service

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Recipe } from './recipe';
-import { RECIPES } from './mock-recipes';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Recipe } from './recipe';
 import { MessageService } from './message.service';
 
 @Injectable({
@@ -9,17 +12,116 @@ import { MessageService } from './message.service';
 })
 export class RecipeService {
 
+  private recipesUrl = 'api/recipes';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient) { }
+
+  /** GET recipes from the server */
   getRecipes(): Observable<Recipe[]> {
-    const recipes = of (RECIPES);
-    this.messageService.add('RecipeService: fetched recipes')
-    return recipes;
+    return this.http.get<Recipe[]>(this.recipesUrl)
+      .pipe(
+        tap(_ => this.log('fetched recipes')),
+        catchError(this.handleError<Recipe[]>('getRecipes', []))
+      );
   }
 
-  getRecipe(id: Number): Observable<Recipe> {
-    const recipe = RECIPES.find(r => r.id == id)!;
-    this.messageService.add(`RecipeService: fetched recipe id=${id}`);
-    return of (recipe);
+  /** GET recipe by id. Return `undefined` when id not found */
+  getRecipeNo404(id: Number): Observable<Recipe> {
+    const url = `${this.recipesUrl}/?id=${id}`;
+    return this.http.get<Recipe[]>(url)
+    .pipe(
+      map(recipes => recipes[0]),
+      tap(r => {
+        const outcome = r ? 'fetched' : 'did not find';
+        this.log(`${outcome} recipe id=${id}`);
+      }),
+      catchError(this.handleError<Recipe>(`getRecipe id=${id}`))
+    );
   }
 
-  constructor(private messageService: MessageService) { }
+  
+  /** GET recipe by id. Will 404 if id not found */
+  getRecipe(id: number): Observable<Recipe> {
+    const url = `${this.recipesUrl}/${id}`;
+    return this.http.get<Recipe>(url).pipe(
+      tap(_ => this.log(`fetched recipe id=${id}`)),
+      catchError(this.handleError<Recipe>(`getRecipe id=${id}`))
+    );
+  }
+
+  /* GET recipes whose name contains search term */
+  searchRecipes(term: string): Observable<Recipe[]> {
+    if (!term.trim()) {
+      // if not search term, return empty recipe array.
+      return of([]);
+    }
+    return this.http.get<Recipe[]>(`${this.recipesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ?
+        this.log(`found recipes matching "${term}"`) :
+        this.log(`no recipes matching "${term}"`)),
+      catchError(this.handleError<Recipe[]>('searchRecipes', []))
+    );
+  }
+
+  //////// Save methods //////////
+
+  /** PUT: update the recipe on the server */
+  updateRecipe(recipe: Recipe): Observable<any> {
+    return this.http.put(this.recipesUrl, recipe, this.httpOptions).pipe(
+      tap(_ => this.log(`updated recipe id=${recipe.id}`)),
+      catchError(this.handleError<any>('updateRecipe'))
+    );
+  }
+
+  /** POST: add a new recipe to the server */
+  addRecipe(recipe: Recipe): Observable<Recipe> {
+    return this.http.post<Recipe>(this.recipesUrl, recipe, this.httpOptions).pipe(
+      tap((newRecipe: Recipe) => this.log(`added recipe w/ id=${newRecipe.id}`)),
+      catchError(this.handleError<Recipe>('addRecipe'))
+    );
+  }
+
+  /** DELETE: delete the recipe from the server */
+  deleteRecipe(id: number): Observable<Recipe> {
+    const url = `${this.recipesUrl}/${id}`;
+
+    return this.http.delete<Recipe>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted recipe id=${id}`)),
+      catchError(this.handleError<Recipe>('deleteRecipe'))
+    );
+  }
+
+
+
+    /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  private log(message: string) {
+    this.messageService.add(`RecipeService: ${message}`);
+  }
+
 }
