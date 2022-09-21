246063d27251f79ae17472eddb39ccf9fa67398c
vmarcell96 <vajdam96@gmail.com>
2022-09-21 13:09:34 +0200

    detail component now on a different route, added go back button

--- src/app/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe';
+import { RecipeService } from '../recipe.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,12 +10,25 @@ import { Recipe } from '../recipe';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  
+
   @Input() recipe? : Recipe;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private recipeService: RecipeService,
+              private location: Location) { }
 
   ngOnInit(): void {
+    this.getRecipe();
+  }
+
+  getRecipe(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.recipeService.getRecipe(id)
+        .subscribe(recipe => this.recipe = recipe);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
