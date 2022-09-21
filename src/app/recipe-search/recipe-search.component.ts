7f7fe99ee5ba65a11e228d28f97747203fd3a6ac
vmarcell96 <vajdam96@gmail.com>
2022-09-21 17:22:37 +0200

    implemented search bar

---
+++ src/app/recipe-search/recipe-search.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Observable, Subject } from 'rxjs';
+
+import {
+   debounceTime, distinctUntilChanged, switchMap
+ } from 'rxjs/operators';
+
+ import { Recipe } from '../recipe';
+ import { RecipeService } from '../recipe.service';
+
+@Component({
+  selector: 'app-recipe-search',
+  templateUrl: './recipe-search.component.html',
+  styleUrls: ['./recipe-search.component.css']
+})
+export class RecipeSearchComponent implements OnInit {
+
+  recipes$!: Observable<Recipe[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private recipeService: RecipeService) { }
+
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.recipes$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.recipeService.searchRecipes(term)),
+    );
+  }
+
+}
