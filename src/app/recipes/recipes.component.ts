b5e2318c4ad5234649d1e708ebc5b034fd65b59b
vmarcell96 <vajdam96@gmail.com>
2022-09-21 11:00:05 +0200

    created onSelect method for recipes

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe';
+import { RECIPES } from '../mock-recipes';
 
 @Component({
   selector: 'app-recipes',
@@ -7,14 +8,18 @@ import { Recipe } from '../recipe';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-
-  recipe: Recipe = {
-    id: 1,
-    name: "Porridge",
-    description: "Put 50g porridge oats in a saucepan, pour in 350ml milk or water and sprinkle in a pinch of salt. Bring to the boil and simmer for 4-5 minutes, stirring from time to time and watching carefully that it doesn't stick to the bottom of the pan. Or you can try this in a microwave."
-  }
+  recipes = RECIPES;
   
   constructor() { }
+
+
+  selectedRecipe?: Recipe;
+
+  onSelect(recipe: Recipe): void {
+  this.selectedRecipe = recipe;
+  console.log(this.selectedRecipe)
+  }
+  
 
   ngOnInit(): void {
     
